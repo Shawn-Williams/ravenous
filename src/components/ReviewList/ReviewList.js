0b357c02c489e93147f19d3472426f235e81cb26
Shawn Williams <scwill35@gmail.com>
2017-11-03 16:11:37 -0500

    Fixed layout issue of review box on smaller screens

--- src/components/ReviewList/ReviewList.js
+++ src/components/ReviewList/ReviewList.js
@@ -6,26 +6,28 @@ import './ReviewList.css'
 class ReviewList extends React.Component {
 
 	componentWillMount() {
-		window.scroll({ top: 600, behavior: 'smooth' });
+		window.scroll({ top: 500, behavior: 'smooth' });
 	}
 
 	componentWillUpdate() {
-		window.scroll({ top: 600, behavior: 'smooth' });
+		window.scroll({ top: 500, behavior: 'smooth' });
 	}
 
 	render() {
 		return (
-			<div className='review-list'>
-				<div className='review-list-header'>
-					<h2>{this.props.name}</h2>
-					<button onClick={this.props.hideReviews}>Close</button>
+			<section className="review-list-wrapper">
+				<div className='review-list'>
+					<div className='review-list-header'>
+						<h2>{this.props.name}</h2>
+						<button onClick={this.props.hideReviews}>Close</button>
+					</div>
+					<div>
+						{this.props.reviews.map((review, index) => {
+							return <Review key={index} review={review}  />
+						})}
+					</div>
 				</div>
-				<div>
-					{this.props.reviews.map((review, index) => {
-						return <Review key={index} review={review}  />
-					})}
-				</div>
-			</div>
+			</section>
 		)
 	}
 }
